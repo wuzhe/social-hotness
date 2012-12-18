b19dcd8bf0d5cc183435b1f475ee53b5884a9054
Zhe Wu <wu@madk.org>
2012-12-19 02:07:59 +0800

    Remove unneeded files since everything is now managed by chrome web store

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
- 
-CHROME=`which chrome`
-${CHROME} --no-message-box --pack-extension=social-hotness --pack-extension-key=social-hotness.pem
-
-echo 'Remember to bump version numbers in manifest.json and updates.xml!'
